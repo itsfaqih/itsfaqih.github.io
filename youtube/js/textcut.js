c75406f5413aca079934e2da3006ac971e55bd69
itsfaqih <48067039+itsfaqih@users.noreply.github.com>
2019-05-28 12:41:44 +0700

    Fixed everything I messed up with
    pls no more buggy thanks

--- youtube/js/textcut.js
+++ youtube/js/textcut.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
     let containerHeight_old = $('#streaming-container').height()
+    let shownDescHeight = $('.video-desc p:nth-of-type(1)').height()
     $('.textcut-btn').click(function() {
         let containerHeight = containerHeight_old
+        let videoDesc = $('.video-desc p')
         let hiddenDesc = $('.video-desc p:nth-of-type(n+2)')
         let hiddenDescHeight = 0
 
-        hiddenDesc.toggleClass('show')
+        videoDesc.toggleClass('show')
 
         hiddenDesc.each(function(index) {
             hiddenDescHeight += ($(this).height() + 16)
         })
         
+        let additionalHeight = hiddenDescHeight + $('.video-desc p:nth-of-type(1)').height() - shownDescHeight
+
         if(hiddenDesc.hasClass('show')){
-            $('#streaming-container').css('height', containerHeight + hiddenDescHeight)
+            $('#streaming-container').css('height', containerHeight + additionalHeight)
         } else {
             $('#streaming-container').css('height', containerHeight)
         }
